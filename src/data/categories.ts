5d912ed0219ddeb11c8b436241ecea83d23d39cb
João Pedro  Bandeira <joao.bandeiraoliveira@gmail.com>
2021-12-28 23:37:56 -0300

    Add files via upload

---
+++ src/data/categories.ts
@@ -0,0 +1,8 @@
+import { category } from "../type/category";
+
+export const categories: category = {
+  Ifood: { title: 'Alimentação', color: 'Red', expense: true },
+  Rent: { title: 'Alugel', color: 'blue', expense: true },
+  Salary: { title: 'Salario', color: 'Green', expense: false },
+
+};
---
+++ src/data/items.ts
@@ -0,0 +1,9 @@
+import { item } from "../type/item";
+
+export const items: item[] = [
+  { data: new Date(2021, 9, 30), category: 'Ifood', title: 'DiVerano', value: 20.5 },
+  { data: new Date(2021, 9, 30), category: 'Ifood', title: 'Açai', value: 15.0 },
+  { data: new Date(2021, 9, 10), category: 'Rent', title: 'Aluguel', value: 250.0 },
+  { data: new Date(2021, 9, 5), category: 'Salary', title: 'Auxilio', value: 650.0 },
+
+];
