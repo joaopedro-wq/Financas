c3f66fbb3a9794c575017404026eced7adb352ee
João Pedro  Bandeira <joao.bandeiraoliveira@gmail.com>
2021-12-28 23:43:16 -0300

    Add files via upload

---
+++ src/componets/tableItem/index.tsx
@@ -0,0 +1,28 @@
+
+import * as C from './style';
+import { item } from '../../type/item';
+import { formatDate } from '../../helpers/dataFilters';
+import { categories } from '../../data/categories';
+
+type Props = {
+  item: item
+}
+
+export const TableItem = ({ item }: Props) => {
+  return (
+    <C.TableLine>
+      <C.TableColumn>{formatDate(item.data)}</C.TableColumn>
+      <C.TableColumn>
+        <C.Category color={categories[item.category].color}>
+          {categories[item.category].title}
+        </C.Category>
+      </C.TableColumn>
+      <C.TableColumn>{item.title}</C.TableColumn>
+      <C.TableColumn>
+        <C.Value color={categories[item.category].expense ? 'red' : 'green'}>
+          R$ {item.value}
+        </C.Value>
+      </C.TableColumn>
+    </C.TableLine>
+  );
+}
